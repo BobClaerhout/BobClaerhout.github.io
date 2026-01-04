4bce8cfa15f85192c56e6e1f101dfc459956b7fe
Bob Claerhout <bob.claerhout@progressus.be>
2026-01-04 23:33:41 +0100

    working mail form

--- raltera/forms/mail.php
+++ raltera/forms/mail.php
@@ -11,26 +11,26 @@ use PHPMailer\PHPMailer\Exception;
 //Load Composer's autoloader (created by composer, not included with PHPMailer)
 require __DIR__ . '/../vendor/autoload.php';
 $mail = new PHPMailer(true);
+//$mail->SMTPDebug  = 1;
 $mail->isSMTP();
-$mail->Host = 'smtp.office365.com';
-$mail->Port       = 587;
+$mail->Host = 'raltera-be.mail.protection.outlook.com';
+$mail->Port       = 25;
 $mail->SMTPSecure = 'tls';
-$mail->SMTPAuth   = true;
+$mail->SMTPAuth   = false;
 $mail->Username = 'bob.claerhout@raltera.be';
-$mail->Password = 'zzzlfsfvtdjhnvxt';
-$mail->SetFrom('bob.claerhout@raltera.be', 'FromEmail');
-$mail->addAddress('bob.claerhout@raltera.be', 'ToEmail');
+$mail->SetFrom('info@raltera.be', 'Contact form Raltera');
+$mail->addReplyTo($_POST['email'], $_POST['name']);
+$mail->addAddress('info@raltera.be', 'Raltera info');
 //$mail->SMTPDebug  = 3;
 //$mail->Debugoutput = function($str, $level) {echo "debug level $level; message: $str";}; //$mail->Debugoutput = 'echo';
 $mail->IsHTML(true);
 
-$mail->Subject = 'Here is the subject';
-$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+$mail->Subject = $_POST['subject'];
+$mail->Body    = $_POST['message'];
 
 if(!$mail->send()) {
     echo 'Message could not be sent.';
     echo 'Mailer Error: ' . $mail->ErrorInfo;
 } else {
-    echo 'Message has been sent';
+    echo 'OK';
 }
